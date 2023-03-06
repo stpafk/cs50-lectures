114819124e637caeec8d73fab93d86ac7dd224f1
stpafk <117909784+stpafk@users.noreply.github.com>
2023-03-06 10:11:51 -0300

    Create conditions2.c

---
+++ math/conditions2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  char c = get_char("Do you agree?");
+  if (c == 'Y' || c == 'y')
+  {
+    printf("Agreed.\n");
+  }
+  else if (c == 'N' || c == 'n');
+  {
+    printf("Not agree.\n");
+  }
+  else 
+  {
+    printf("Please respond Y/N\n");
+}
