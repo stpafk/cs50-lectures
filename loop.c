24e6977d5264b6c3c4b990240771f73ba2f16550
stpafk <117909784+stpafk@users.noreply.github.com>
2023-03-06 10:22:38 -0300

    Create loop.c

---
+++ loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int i = 0;
+
+int main(void)
+{
+  while (i < 50)
+  {
+    printf("Hello World.\n");
+    i++;
+  }
+}
+//or 
+
+int main_2(void)
+{
+  for (int i = 0; i < 50; i++)
+  {
+    printf("Hello World.\n);
+  }
+}
