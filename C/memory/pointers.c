c07f77379c6f23077c7611b5bb44d1802f74accc
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-04-10 12:37:06 +0000

    make files

--- C/memory/pointers.c
+++ C/memory/pointers.c
@@ -4,5 +4,6 @@ int main(void)
 {
     int n = 50; //4 bytes
     int *p = &n; // pointer, which takes 8 bytes and points at the location of n
-    printf("%p\n", p); // 
+    printf("%p\n", p); // pointers works in a way that it tells where in memory such adress is located
+    //and it is in this addres that we can make changes
 } 
