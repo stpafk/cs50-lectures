08a03b77ba6f435af79c9cc35ac554bbac6cf849
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-04-10 12:48:30 +0000

    compare c

--- C/memory/strings.c
+++ C/memory/strings.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
 
+/*
 int main(void)
 {
     string s = "HI!";
-    printf("%p\n", s[0]);
+    printf("%p\n", s[0]); // points to first adress of memory
+}
+*/
+
+int main(void) 
+{
+    char *s = "HI!";
+    printf("%c\n", *s); // pointers gets the first index because of it's relations
+    printf("%c\n", *(s+1)); // get address + 1
+    printf("%c\n", *(s+2));
+    //printf("%c\n", *(s+100000)) returns "segmentation fault", which means there is something wrong with the memory allocation
+    
 }
